dd5aa8a7bbf9647c730050aa85a708e5db51060c
Polina <gulienko.polina@mail.ru>
2024-09-15 19:26:14 +0500

    Task2

---
+++ Tyuiu.GulienkoPO.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.GulienkoPO.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.GulienkoPO.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Полина";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет..., Полина", res);
+        }
+    }
+}
